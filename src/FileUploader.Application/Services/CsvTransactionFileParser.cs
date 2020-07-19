2d04dcd95a8d824f8a8d42656f70873cafb410fb
OlesKorzhynskiy <oles.korzhynskiy@gmail.com>
2020-07-19 21:12:13 +0300

    Implemented CsvFileParser

--- src/FileUploader.Application/Services/CsvTransactionFileParser.cs
+++ src/FileUploader.Application/Services/CsvTransactionFileParser.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.TypeConversion;
 using FileUploader.Application.Interfaces;
 using FileUploader.Domain.Entities;
 using Microsoft.AspNetCore.Http;
@@ -9,7 +15,21 @@ namespace FileUploader.Application.Services
     {
         public IEnumerable<Transaction> Parse(IFormFile file)
         {
-            throw new System.NotImplementedException();
+            var stream = file.OpenReadStream();
+            using var reader = new StreamReader(stream);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            var options = new TypeConverterOptions
+            {   
+                Formats = new [] { "dd/MM/yyyy hh:mm:ss" }
+            };
+
+            csv.Configuration.TypeConverterOptionsCache.AddOptions<DateTime>(options);
+            csv.Configuration.HasHeaderRecord = false;
+
+            var records = csv.GetRecords<Transaction>();
+
+            return records.ToList();
         }
     }
 }
